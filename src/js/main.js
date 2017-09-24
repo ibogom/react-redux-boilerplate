7b39be3c942c0c11006dc12663cc2c9cec7e62dc
Ievgen <ievgen.bogomolov@gmail.com>
2017-09-24 17:28:19 +0300

    Add stores folder

--- src/js/main.js
+++ src/js/main.js
@@ -2,19 +2,16 @@
 import React from 'react';
 import {render} from 'react-dom'
 import {Provider} from 'react-redux';
-import {createStore} from 'redux';
 import {BrowserRouter as Router} from 'react-router-dom';
 
 /** CONTAINERS **/
 import {App} from './containers';
 
-/** REDUCERS **/
-import AppReducers from './reducers';
+/** STORES **/
+import store from './stores';
 
 /** THEMES **/
 import '../assets/themes/default/main.scss';
-
-const store = createStore(AppReducers);
 
 render(<Provider store={store}>
     <Router>
